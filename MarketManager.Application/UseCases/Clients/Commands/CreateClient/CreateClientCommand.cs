a66d73c0bb878a798f3ddf1d16127a9843c42575
acer <maqsudhaydarov1203@gmail.com>
2023-07-19 20:42:07 +0500

    comitted

--- MarketManager.Application/UseCases/Clients/Commands/CreateClient/CreateClientCommand.cs
+++ MarketManager.Application/UseCases/Clients/Commands/CreateClient/CreateClientCommand.cs
@@ -1,41 +1,31 @@
-﻿using MarketManager.Application.Common.Interfaces;
+﻿using AutoMapper;
+using MarketManager.Application.Common.Interfaces;
 using MarketManager.Domain.Entities;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace MarketManager.Application.UseCases.Clients.Commands.CreateClient;
 
-namespace MarketManager.Application.UseCases.Clients.Commands.CreateClient
+public class CreateClientCommand:IRequest<Guid>
 {
-    public class CreateClientCommand:IRequest<Guid>
+    public double TotalPrice { get; set; }
+    public double Discount { get; set; }
+}
+public class CreateClientCommandHandler:IRequestHandler<CreateClientCommand, Guid>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public CreateClientCommandHandler(IApplicationDbContext dbContext, IMapper mapper)
     {
-        public double TotalPrice { get; set; }
-        public double Discount { get; set; }
+        _dbContext = dbContext;
+        _mapper = mapper;
     }
-    public class CreateClientCommandHandler:IRequestHandler<CreateClientCommand, Guid>
+
+    public async Task<Guid> Handle(CreateClientCommand request, CancellationToken cancellationToken)
     {
-        private readonly IApplicationDbContext _dbContext;
+       Client client = _mapper.Map<Client>(request);
 
-        public CreateClientCommandHandler(IApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public async Task<Guid> Handle(CreateClientCommand request, CancellationToken cancellationToken)
-        {
-            var newClient = new Client
-            {
-                Id = Guid.NewGuid(),
-                TotalPrice = request.TotalPrice,
-                Discount = request.Discount
-            };
-
-            _dbContext.Clients.Add(newClient);
-            await _dbContext.SaveChangesAsync(cancellationToken);
-            return newClient.Id;
-        }
+        await _dbContext.Clients.AddAsync(client, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return client.Id;
     }
 }
