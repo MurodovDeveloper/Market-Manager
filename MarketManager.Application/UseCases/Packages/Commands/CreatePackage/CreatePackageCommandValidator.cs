47e80fa2f8d380bab14ff05309bf906f17824a29
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-20 08:21:07 +0500

    save

---
+++ MarketManager.Application/UseCases/Packages/Commands/CreatePackage/CreatePackageCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace MarketManager.Application.UseCases.Packages.Commands.CreatePackage
+{
+    public class CreatePackageCommandValidator : AbstractValidator<CreatePackageCommand>
+    {
+        public CreatePackageCommandValidator()
+        {
+            RuleFor(t => t.ProductId).NotEmpty()
+                .NotNull()
+                .WithMessage("Product id is required.");
+
+            RuleFor(t => t.SupplierId).NotEmpty()
+                .NotNull()
+                .WithMessage("Supplier id is required.");
+
+            RuleFor(t => t.IncomingCount)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("IncomingCount is required.");
+
+            RuleFor(t => t.IncomingPrice)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("IncomingPrice is required.");
+
+            RuleFor(t => t.SalePrice)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("SalePrice is required.");
+
+            RuleFor(t => t.IncomingDate)
+                .NotNull()
+                .WithMessage("Working date is required.");
+        }
+    }
+}
