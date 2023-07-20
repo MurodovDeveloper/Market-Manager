3684224f067db968eb6512434885727cd893e406
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-20 22:48:42 +0500

    fixed Muhammadqodir's bug

--- MarketManager.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
+++ MarketManager.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using MarketManager.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MarketManager.Infrastructure.Persistence.Configurations;
 public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
