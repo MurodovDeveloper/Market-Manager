12f4b9ece1dd912e73ff27134d12c529007662f9
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-20 10:05:43 +0500

    added configuration

--- MarketManager.Infrastructure/Persistence/Configurations/ProductTypeConfiguration.cs
+++ MarketManager.Infrastructure/Persistence/Configurations/ProductTypeConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MarketManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace MarketManager.Infrastructure.Persistence.Configurations
 {
-    internal class ProductTypeConfiguration
+    public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
     {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(t => t.Name)
+              .HasMaxLength(70)
+              .IsRequired();
+        }
     }
+
+   
 }
