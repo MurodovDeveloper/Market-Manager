1874e130778dcc4c59e603c7b2dc577b5bd4ff4f
acer <maqsudhaydarov1203@gmail.com>
2023-07-20 09:46:35 +0500

    add maping for client

---
+++ MarketManager.Application/Common/Mappings/ClientMapping.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MarketManager.Application.UseCases.Clients.Commands.CreateClient;
+using MarketManager.Application.UseCases.Clients.Commands.DeleteClient;
+using MarketManager.Application.UseCases.Clients.Commands.UpdateClient;
+using MarketManager.Application.UseCases.Clients.Queries.GetAllClients;
+using MarketManager.Application.UseCases.Clients.Queries.GetClientById;
+using MarketManager.Domain.Entities;
+namespace MarketManager.Application.Common.Mappings;
+
+public class ClientMapping:Profile
+{
+    public ClientMapping()
+    {
+        ClientMap();
+    }
+
+    private void ClientMap()
+    {
+        CreateMap<CreateClientCommand, Client>();
+        CreateMap<UpdateClientCommand, Client>();
+        CreateMap<DeleteClientCommand, Client>();
+        CreateMap<GetAllClientsQuery, Client>();
+        CreateMap<GetClientByIdQuery, Client>();
+    }
+}
