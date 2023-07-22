01665780ea7b21141dc3241b3f3f29c97ab5a57e
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-22 11:40:21 +0500

    save

--- MarketManager.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommand.cs
+++ MarketManager.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -5,5 +5,29 @@ using MediatR;
 
 namespace MarketManager.Application.UseCases.Products.Commands.CreateProduct
 {
-  
+    public class CreateProductCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid ProductTypeId { get; set; }
+    }
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public CreateProductCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            Product Product = _mapper.Map<Product>(request);
+            await _context.Products.AddAsync(Product, cancellationToken);
+            await _context.SaveChangesAsync();
+            return Product.Id;
+        }
+    }
 }
