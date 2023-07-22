79ae5ae40cc0af5120f2147357d8730cd9bbd705
Khojiakbar <77355381+Khamrakulov-Khojiakbar@users.noreply.github.com>
2023-07-22 14:46:29 +0500

    Forgotten controller added

---
+++ MarketManager.Application/Common/Mappings/ProductTypesMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MarketManager.Application.UseCases.Clients.Queries.GetAllClients;
+using MarketManager.Application.UseCases.ExpiredProducts.Queries;
+using MarketManager.Application.UseCases.Products.Commands.DeleteProduct;
+using MarketManager.Application.UseCases.Products.Commands.UpdateProduct;
+using MarketManager.Application.UseCases.ProductTypes.Commands.CreateProductsType;
+using MarketManager.Domain.Entities;
+
+namespace MarketManager.Application.Common.Mappings;
+
+public class ProductTypesMappingProfile : Profile
+{
+    public ProductTypesMappingProfile()
+    {
+        CreateMap<CreateProductTypeCommand, ProductType>().ReverseMap();
+        CreateMap<UpdateProductCommand, ProductType>().ReverseMap();
+        CreateMap<DeleteProductCommand, ProductType>().ReverseMap();
+        CreateMap<GetAllClientsQueryResponse, ProductType>().ReverseMap();
+        CreateMap<GetByIdExpiredProductsResponce, ProductType>().ReverseMap();
+    }
+}
