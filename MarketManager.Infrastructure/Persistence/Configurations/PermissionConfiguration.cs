cca41c7d788ae333ba39af8337ed2ccc431859c2
Muhammadqodir <111755347+Azamov-Muhammadqodir@users.noreply.github.com>
2023-07-22 12:08:29 +0500

    add pemission Configuration

---
+++ MarketManager.Infrastructure/Persistence/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MarketManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManager.Infrastructure.Persistence.Configurations
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.Property(t => t.Name)
+             .HasMaxLength(50)
+             .IsRequired();
+        }
+    }
+}
+
