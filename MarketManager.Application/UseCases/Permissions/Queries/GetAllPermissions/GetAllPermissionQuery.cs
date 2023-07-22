85450932acaffb0cd8c4e79b5e261b91a5a5a5db
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-22 12:33:02 +0300

    added generic PaginatedList, let everyone use this. It has done for permission

--- MarketManager.Application/UseCases/Permissions/Queries/GetAllPermissions/GetAllPermissionQuery.cs
+++ MarketManager.Application/UseCases/Permissions/Queries/GetAllPermissions/GetAllPermissionQuery.cs
@@ -1,34 +1,38 @@
 ﻿using AutoMapper;
 using MarketManager.Application.Common.Interfaces;
+using MarketManager.Application.Common.Models;
 using MarketManager.Application.UseCases.Permissions.ResponseModels;
 using MarketManager.Domain.Entities;
 using MediatR;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MarketManager.Application.UseCases.Permissions.Queries.GetAllPermissions
+namespace MarketManager.Application.UseCases.Permissions.Queries.GetAllPermissions;
+
+public record GetAllPermissionQuery : IRequest<PaginatedList<PermissionResponse>>
 {
-    public record GetAllPermissionQuery:IRequest<List<PermissionResponse>>;
-    public class GetAllPermissionQueryHandler : IRequestHandler<GetAllPermissionQuery, List<PermissionResponse>>
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
+public class GetAllPermissionQueryHandler : IRequestHandler<GetAllPermissionQuery, PaginatedList<PermissionResponse>>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+    public GetAllPermissionQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
     {
-        private readonly IApplicationDbContext _dbContext;
-        private readonly IMapper _mapper;
-        public GetAllPermissionQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
-        {
-            _dbContext = dbContext;
-            _mapper = mapper;
-        }
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
 
-        public async Task<List<PermissionResponse>> Handle(GetAllPermissionQuery request, CancellationToken cancellationToken)
-        {
-            var permissions = await _dbContext.Permissions.ToListAsync(cancellationToken);
-            var result = _mapper.Map<List<PermissionResponse>>(permissions);
-            return result;
-        }
+    public async Task<PaginatedList<PermissionResponse>> Handle(GetAllPermissionQuery request, CancellationToken cancellationToken)
+    {
+        var pageSize = request.PageSize;
+        var pageNumber = request.PageNumber;
+
+        var permissions = _dbContext.Permissions.AsQueryable();
+        var paginatedPermissions = await PaginatedList<Permission>.CreateAsync(permissions, pageNumber, pageSize);
+
+        var permissionResponses = _mapper.Map<List<PermissionResponse>>(paginatedPermissions.Items);
+
+        var result = new PaginatedList<PermissionResponse>(permissionResponses, paginatedPermissions.TotalCount, paginatedPermissions.PageNumber, paginatedPermissions.TotalPages);
+        return result;
     }
 }
