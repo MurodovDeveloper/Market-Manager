1382bb075ee4a7186811f71053c6ea8b2e26ece8
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-22 19:40:26 +0500

    User's excel report converted to mediator

--- MarketManager.API/Controllers/ReportsController.cs
+++ MarketManager.API/Controllers/ReportsController.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using MarketManager.Application.UseCases.Users.Queries.GetAllUser;
+using MarketManager.Application.UseCases.Users.Report;
 using MarketManager.Application.UseCases.Users.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,64 +18,9 @@ public class ReportsController : BaseApiController
     [HttpPost]
     public async Task<FileResult> ExportExcel(string fileName = "users")
     {
-       
-        using (XLWorkbook wb = new XLWorkbook())
-        {
-            var userData =await GetUsersAsync();
-            var sheet1 = wb.AddWorksheet(userData, "Users");
+       var result = await _mediator.Send(new GetUsersExcel { FileName = fileName });
+        return File(result.FileContents, result.Option, result.FileName);
+    }
     
-
-            sheet1.Column(1).Style.Font.FontColor = XLColor.Red;
-        
-            sheet1.Columns(2, 4).Style.Font.FontColor = XLColor.Blue;
-
-            sheet1.Row(1).CellsUsed().Style.Fill.BackgroundColor = XLColor.Black;
-      
-            sheet1.Row(1).Style.Font.FontColor = XLColor.White;
-
-            sheet1.Row(1).Style.Font.Bold = true;
-            sheet1.Row(1).Style.Font.Shadow = true;
-            sheet1.Row(1).Style.Font.Underline = XLFontUnderlineValues.Single;
-            sheet1.Row(1).Style.Font.VerticalAlignment = XLFontVerticalTextAlignmentValues.Superscript;
-            sheet1.Row(1).Style.Font.Italic = true;
-
-            sheet1.RowHeight = 20;
-            sheet1.Column(1).Width = 38;
-            sheet1.Column(2).Width = 20;
-            sheet1.Column(3).Width = 20;
-            sheet1.Column(4).Width = 20;
-            
-          
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                wb.SaveAs(ms);
-                return File(ms.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{fileName}.xlsx");
-            }
-        }
-    }
-    [NonAction]
-    private async Task<DataTable> GetUsersAsync()
-    {
-        DataTable dt = new DataTable();
-        dt.TableName = "Empdata";
-        dt.Columns.Add("Code", typeof(Guid));
-        dt.Columns.Add("Name", typeof(string));
-        dt.Columns.Add("Username", typeof(string));
-        dt.Columns.Add("Phone", typeof(string));
-        
-
-        var _list =await _mediator.Send(new GetAllUserQuery());
-        if (_list.Count > 0)
-        {
-            _list.ForEach(item =>
-            {
-                dt.Rows.Add(item.Id, item.FullName, item.Username, item.Phone);
-               
-            });
-        }
-
-        return await Task.FromResult(dt);
-    }
-
+   
 }
