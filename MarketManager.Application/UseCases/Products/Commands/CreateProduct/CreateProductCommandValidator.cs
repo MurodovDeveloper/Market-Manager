e8886344496280026115dab31a77a1459c72aab0
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-22 11:45:20 +0500

    save

--- MarketManager.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
+++ MarketManager.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -6,7 +6,19 @@ namespace MarketManager.Application.UseCases.Products.Commands.CreateProduct
     {
         public CreateProductCommandValidator()
         {
-          
+            RuleFor(t => t.ProductTypeId).NotEmpty()
+                .NotNull()
+                .WithMessage("Product Type id is required.");
+
+            RuleFor(d => d.Name)
+                .NotEmpty()
+                .MaximumLength(100)
+                .WithMessage("Name is required");
+
+            RuleFor(d => d.Description)
+                .NotEmpty()
+                .MaximumLength(250)
+                .WithMessage("Description is required");
         }
     }
 }
