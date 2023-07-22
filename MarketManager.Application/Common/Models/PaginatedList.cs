2870d52b5682abf1e6962952a7f1a0e6d1969de5
Bekzod <bekzod_sobirov@list.ru>
2023-07-22 14:22:22 +0500

    added pagination

--- MarketManager.Application/Common/Models/PaginatedList.cs
+++ MarketManager.Application/Common/Models/PaginatedList.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MarketManager.Application.UseCases.Packages.Queries.GetAllWithPagination
+namespace MarketManager.Application.Common.Models
 {
-    public class GetAllPackageWithPagination
+    internal class PaginatedList
     {
-
     }
 }
