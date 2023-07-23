0c2efde09208ac940a8bbb40e9c9c75eaa0458dc
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-23 09:58:18 +0500

    Package Mapping added

---
+++ MarketManager.Application/Common/Mappings/PackageMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MarketManager.Application.UseCases.Packages.Commands.CreatePackage;
+using MarketManager.Application.UseCases.Packages.Commands.DeletePackage;
+using MarketManager.Application.UseCases.Packages.Commands.UpdatePackage;
+using MarketManager.Application.UseCases.Packages.Queries.GetAllPackages;
+using MarketManager.Application.UseCases.Packages.Queries.GetPackageById;
+using MarketManager.Domain.Entities;
+
+namespace MarketManager.Application.Common.Mappings
+{
+    public class PackageMapping:Profile
+    {
+        public PackageMapping()
+        {
+            CreateMap<CreatePackageCommand, Package>();
+            CreateMap<DeletePackageCommand, Package>();
+            CreateMap<UpdatePackageCommand, Package>();
+
+            CreateMap<Package, GetAllPackagesQueryResponse>();
+            CreateMap<Package, GetPackageByIdQuery>();
+        }
+    }
+}
