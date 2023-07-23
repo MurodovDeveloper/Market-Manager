7425aa031973b8780e6a917eb0e09544f760a1a0
GetSomeHelp <otajonovzafar2003@gmail.com>
2023-07-23 19:48:36 +0500

    added searching for Suppliers

--- MarketManager.Application/UseCases/Suppliers/Queries/GetAllSuppliers/GetAllSuppliersQuery.cs
+++ MarketManager.Application/UseCases/Suppliers/Queries/GetAllSuppliers/GetAllSuppliersQuery.cs
@@ -3,6 +3,7 @@ using MarketManager.Application.Common.Interfaces;
 using MarketManager.Application.Common.Models;
 using MarketManager.Application.UseCases.Clients.Queries.GetAllClients;
 using MarketManager.Domain.Entities;
+using MarketManager.Domain.Entities.Identity;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ namespace MarketManager.Application.UseCases.Suppliers.Queries.GetAllSuppliers;
 
 public record GetAllSuppliersQuery : IRequest<PaginatedList<GetAllSuppliersQueryResponse>>
 {
-    public int ListId { get; init; }
+    public string? SearchTerm { get; init; }
     public int PageNumber { get; init; } = 1;
     public int PageSize { get; init; } = 10;
 }
@@ -27,6 +28,17 @@ public class GetAllSuppliersQueryHandler : IRequestHandler<GetAllSuppliersQuery,
 
     public async Task<PaginatedList<GetAllSuppliersQueryResponse>> Handle(GetAllSuppliersQuery request, CancellationToken cancellationToken)
     {
+
+        var search = request.SearchTerm?.Trim();
+        var suppliers = _context.Suppliers.AsQueryable();
+        if (!string.IsNullOrEmpty(search))
+        {
+            suppliers = suppliers.Where(s=>s.Name.ToLower().Contains(search.ToLower()));
+        }
+        if (suppliers is null || suppliers.Count() <= 0)
+        {
+            throw new NotFoundException(nameof(Supplier), search);
+        }
         var query = _context.Suppliers
             .Select(s => _mapper.Map<Supplier, GetAllSuppliersQueryResponse>(s)) ;
         return await PaginatedList<GetAllSuppliersQueryResponse>.CreateAsync(query, request.PageNumber, request.PageSize);
