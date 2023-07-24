c93001173c5b3f390f7d6635381e32bce88d18ac
GetSomeHelp <otajonovzafar2003@gmail.com>
2023-07-25 02:31:42 +0500

    added filter for suppliers

--- MarketManager.Application/UseCases/Suppliers/Filters/SuppliersFilterCommand.cs
+++ MarketManager.Application/UseCases/Suppliers/Filters/SuppliersFilterCommand.cs
@@ -2,56 +2,50 @@
 using MarketManager.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MarketManager.Application.UseCases.Suppliers.Filters
+namespace MarketManager.Application.UseCases.Suppliers.Filters;
+
+public class SuppliersFilterCommand : IRequest<List<Supplier>>
 {
-    public class SuppliersFilterCommand : IRequest<List<Supplier>>
+    public DateOnly? StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+    public bool OrderByName { get; set; } = false;
+    public bool OrderByPhone { get; set;} = false;
+}
+public class SuppliersFilterCommandHandler : IRequestHandler<SuppliersFilterCommand, List<Supplier>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public SuppliersFilterCommandHandler(IApplicationDbContext context)
     {
-        public DateOnly? StartDate { get; set; }
-        public DateOnly? EndDate { get; set; }
-        public bool OrderByName { get; set; } = false;
-        public bool OrderByPhone { get; set;} = false;
+        _context = context;
     }
-    public class SuppliersFilterCommandHandler : IRequestHandler<SuppliersFilterCommand, List<Supplier>>
+
+    public async Task<List<Supplier>> Handle(SuppliersFilterCommand request, CancellationToken cancellationToken)
     {
-        private readonly IApplicationDbContext _context;
+        if (request.StartDate is null)
+            request.StartDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-30));
 
-        public SuppliersFilterCommandHandler(IApplicationDbContext context)
+        if (request.EndDate is null)
+            request.EndDate = DateOnly.FromDateTime(DateTime.Now);
+
+        if (request.OrderByName)
         {
-            _context = context;
+            return await _context.Suppliers
+                .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
+                .OrderBy(x => x.Name).ToListAsync(cancellationToken);
         }
-
-        public async Task<List<Supplier>> Handle(SuppliersFilterCommand request, CancellationToken cancellationToken)
+        else if (request.OrderByPhone)
         {
-            if (request.StartDate is null)
-                request.StartDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-30));
-
-            if (request.EndDate is null)
-                request.EndDate = DateOnly.FromDateTime(DateTime.Now);
-
-            if (request.OrderByName)
-            {
-                return await _context.Suppliers
-                    .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
-                    .OrderBy(x => x.Name).ToListAsync(cancellationToken);
-            }
-            else if (request.OrderByPhone)
-            {
-                return await _context.Suppliers
-                    .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
-                    .OrderBy(x => x.Phone).ToListAsync(cancellationToken);
-            }
-            else
-            {
-                return await _context.Suppliers
-                    .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
-                    .OrderBy(x => x.Name).ToListAsync(cancellationToken);
-            }
+            return await _context.Suppliers
+                .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
+                .OrderBy(x => x.Phone).ToListAsync(cancellationToken);
+        }
+        else
+        {
+            return await _context.Suppliers
+                .Where(supplier => DateOnly.FromDateTime(supplier.CreatedDate) >= request.StartDate && DateOnly.FromDateTime(supplier.CreatedDate) <= request.EndDate)
+                .OrderBy(x => x.Name).ToListAsync(cancellationToken);
         }
     }
 }
