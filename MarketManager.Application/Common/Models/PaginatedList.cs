774ab58a04f2e1553cf22012b74211d56cd68444
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-24 06:01:03 +0500

    save

--- MarketManager.Application/Common/Models/PaginatedList.cs
+++ MarketManager.Application/Common/Models/PaginatedList.cs
@@ -3,27 +3,25 @@
 namespace MarketManager.Application.Common.Models;
 public class PaginatedList<T>
 {
-    public IReadOnlyCollection<T> Items { get; }
-    public int PageNumber { get; }
-    public int TotalPages { get; }
-    public int TotalCount { get; }
+    public int PageNumber { get; private set; }
+    public int TotalPages { get; private set; }
+    public int TotalCount { get; private set; }
+    public List<T> Items { get; private set; }
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
 
-    public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
+    public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
     {
-        Items = items;
-        TotalCount = pageSize;
         PageNumber = pageNumber;
-        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        TotalPages = (int)Math.Ceiling(count / (decimal)pageSize);
+        TotalCount = count;
+        Items = items;
     }
 
-    public bool HasPreviousPage => PageNumber > 1;
-    public bool HasNextPage => PageNumber < TotalCount;
-
-    public static PaginatedList<T> CreateAsync(IEnumerable<T> source, int pageNumber, int pageSize)
+    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T>? source, int pageNumber, int pageSize)
     {
-        var count = source.Count();
-        var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-
+        var count = await source.CountAsync();
+        List<T> items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
         return new PaginatedList<T>(items, count, pageNumber, pageSize);
     }
 }
