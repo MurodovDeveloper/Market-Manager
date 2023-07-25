2018d43036d7f29a2a1054534c2b130d0dfafaa5
Dilshod <dilshodsh97@gmail.com>
2023-07-25 09:27:40 +0500

    UseCases.Orders.Import.Export fixed

--- MarketManager.Application/UseCases/Orders/Import.Export/AddOrdersFromPDF.cs
+++ MarketManager.Application/UseCases/Orders/Import.Export/AddOrdersFromPDF.cs
@@ -7,9 +7,7 @@ using Spire.Pdf;
 using Spire.Pdf.Utilities;
 using static MarketManager.Application.UseCases.Orders.Queries.GetAllOrders.GetallOrderCommmandHandler;
 
-namespace MarketManager.Application.UseCases.Orders.Import.Export
-{
-    public record AddOrdersFromPDF(IFormFile PdfFile) : IRequest<List<OrderResponse>>;
+namespace MarketManager.Application.UseCases.Orders.Import.Export;
 
 public record AddOrdersFromPDF(IFormFile PdfFile) : IRequest<List<OrderResponse>>;
 
@@ -26,28 +24,28 @@ public class AddOrdersFromPDFHandler : IRequestHandler<AddOrdersFromPDF, List<Or
         _mapper = mapper;
     }
 
-        public async Task<List<OrderResponse>> Handle(AddOrdersFromPDF request, CancellationToken cancellationToken)
+    public async Task<List<OrderResponse>> Handle(AddOrdersFromPDF request, CancellationToken cancellationToken)
+    {
+        byte[] PdfInBytes = ConvertFormFileToByteArray(request.PdfFile);
+        PdfDocument pdf = new(PdfInBytes);
+        List<OrderResponse> result = new();
+        for (int pageIndex = 0; pageIndex < pdf.Pages.Count; pageIndex++)
         {
-             byte[] PdfInBytes = ConvertFormFileToByteArray(request.PdfFile);
-            PdfDocument pdf = new(PdfInBytes);
-            List<OrderResponse> result = new();
-            for (int pageIndex = 0; pageIndex < pdf.Pages.Count; pageIndex++)
+            PdfTableExtractor extractor = new(pdf);
+            PdfTable[] tableLists = extractor.ExtractTable(pageIndex);
+            if (tableLists != null && tableLists.Length > 0)
             {
-                PdfTableExtractor extractor = new PdfTableExtractor(pdf);
-                PdfTable[] tableLists = extractor.ExtractTable(pageIndex);
-                if (tableLists != null && tableLists.Length > 0)
+                foreach (PdfTable table in tableLists)
                 {
-                    foreach (PdfTable table in tableLists)
+                    for (int i = 2; i < table.GetRowCount(); i++)
                     {
-                        for (int i = 2; i < table.GetRowCount(); i++)
+                        Order order = new()
                         {
-                            Order order = new()
-                            {
-                                TotalPrice = decimal.Parse(table.GetText(i, 1)),
-                                CardPriceSum = decimal.Parse(table.GetText(i, 2)),
-                                CashPurchaseSum = decimal.Parse(table.GetText(i, 3)),
-                                ClientId = Guid.Parse(table.GetText(i, 4).Replace("\n",""))
-                            };
+                            TotalPrice = decimal.Parse(table.GetText(i, 1)),
+                            ItemPriceSum = decimal.Parse(table.GetText(i, 2)),
+                            ItemPurchaseSum = decimal.Parse(table.GetText(i, 3)),
+                            ClientId = Guid.Parse(table.GetText(i, 4).Replace("\n", ""))
+                        };
 
                         await _context.Orders.AddAsync(order);
                         result.Add(_mapper.Map<OrderResponse>(order));
@@ -60,10 +58,8 @@ public class AddOrdersFromPDFHandler : IRequestHandler<AddOrdersFromPDF, List<Or
     }
     public byte[] ConvertFormFileToByteArray(IFormFile file)
     {
-        using (MemoryStream ms = new())
-        {
-            file.CopyTo(ms);
-            return ms.ToArray();
-        }
+        using MemoryStream ms = new();
+        file.CopyTo(ms);
+        return ms.ToArray();
     }
 }
